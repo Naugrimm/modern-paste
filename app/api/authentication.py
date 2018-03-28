f48d6bf1fe88b313c990a98e499646f87596494e
Erik Anders <erik.anders@securepoint.de>
2018-03-28 14:46:09 +0200

    format code

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -16,57 +16,57 @@ from util.exception import *
 @app.route(LoginUserURI.path, methods=['POST'])
 @require_form_args(['username', 'password'])
 def login_user():
-    """
-    Authenticate and log in a user.
-    """
-    data = request.get_json()
-    success_resp = flask.jsonify({
-        constants.api.RESULT: constants.api.RESULT_SUCCESS,
-        constants.api.MESSAGE: None,
-        'username': data['username'],
-    }), constants.api.SUCCESS_CODE
+  """
+  Authenticate and log in a user.
+  """
+  data = request.get_json()
+  success_resp = flask.jsonify({
+      constants.api.RESULT: constants.api.RESULT_SUCCESS,
+      constants.api.MESSAGE: None,
+      'username': data['username'],
+  }), constants.api.SUCCESS_CODE
 
-    if current_user.is_authenticated:
-        # Already logged in; no action is required
-        return success_resp
-
-    try:
-        if not database.user.authenticate_user(data['username'], data['password']):
-            return flask.jsonify(constants.api.AUTH_FAILURE), constants.api.AUTH_FAILURE_CODE
-    except UserDoesNotExistException:
-        return flask.jsonify(constants.api.NONEXISTENT_USER_FAILURE), constants.api.NONEXISTENT_USER_FAILURE_CODE
-
-    login(
-        user=database.user.get_user_by_username(data['username']),
-        remember=bool(data.get('remember_me', False)),
-    )
+  if current_user.is_authenticated:
+    # Already logged in; no action is required
     return success_resp
+
+  try:
+    if not database.user.authenticate_user(data['username'], data['password']):
+      return flask.jsonify(constants.api.AUTH_FAILURE), constants.api.AUTH_FAILURE_CODE
+  except UserDoesNotExistException:
+    return flask.jsonify(constants.api.NONEXISTENT_USER_FAILURE), constants.api.NONEXISTENT_USER_FAILURE_CODE
+
+  login(
+      user=database.user.get_user_by_username(data['username']),
+      remember=bool(data.get('remember_me', False)),
+  )
+  return success_resp
 
 
 @app.route(LogoutUserURI.path, methods=['POST'])
 @login_required
 def logout_user():
-    """
-    Log the current user out, as applicable.
-    """
-    username = str(current_user.username)
-    logout()
-    return flask.jsonify({
-        constants.api.RESULT: constants.api.RESULT_SUCCESS,
-        constants.api.MESSAGE: None,
-        'username': username,
-    }), constants.api.SUCCESS_CODE
+  """
+  Log the current user out, as applicable.
+  """
+  username = str(current_user.username)
+  logout()
+  return flask.jsonify({
+      constants.api.RESULT: constants.api.RESULT_SUCCESS,
+      constants.api.MESSAGE: None,
+      'username': username,
+  }), constants.api.SUCCESS_CODE
 
 
 @app.route(AuthStatusURI.path, methods=['POST'])
 def auth_status():
-    """
-    Gets the authentication status for the current user, if any.
-    """
-    return flask.jsonify({
-        'is_authenticated': bool(current_user.is_authenticated),
-        'user_details': {
-            'username': getattr(current_user, 'username', None),
-            'user_id': getattr(current_user, 'user_id', None),
-        },
-    }), constants.api.SUCCESS_CODE
+  """
+  Gets the authentication status for the current user, if any.
+  """
+  return flask.jsonify({
+      'is_authenticated': bool(current_user.is_authenticated),
+      'user_details': {
+          'username': getattr(current_user, 'username', None),
+          'user_id': getattr(current_user, 'user_id', None),
+      },
+  }), constants.api.SUCCESS_CODE
--- app/models/user.py
+++ app/models/user.py
@@ -5,42 +5,42 @@ from modern_paste import db
 
 
 class User(db.Model):
-    __tablename__ = 'user'
+  __tablename__ = 'user'
 
-    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    is_active = db.Column(db.Boolean)
-    signup_time = db.Column(db.Integer)
-    signup_ip = db.Column(db.Text)
-    username = db.Column(db.String(64), index=True)
-    password_hash = db.Column(db.Text)
-    name = db.Column(db.Text, default=None)
-    email = db.Column(db.Text, default=None)
-    api_key = db.Column(db.String(64), index=True)
+  user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  is_active = db.Column(db.Boolean)
+  signup_time = db.Column(db.Integer)
+  signup_ip = db.Column(db.Text)
+  username = db.Column(db.String(64), index=True)
+  password_hash = db.Column(db.Text)
+  name = db.Column(db.Text, default=None)
+  email = db.Column(db.Text, default=None)
+  api_key = db.Column(db.String(64), index=True)
 
-    def __init__(
-        self,
-        signup_ip,
-        username,
-        password_hash,
-        name,
-        email,
-    ):
-        self.is_active = True
-        self.signup_time = time.time()
-        self.signup_ip = signup_ip
-        self.username = username
-        self.password_hash = password_hash
-        self.name = name
-        self.email = email
-        self.api_key = util.testing.random_alphanumeric_string(length=64)
+  def __init__(
+      self,
+      signup_ip,
+      username,
+      password_hash,
+      name,
+      email,
+  ):
+    self.is_active = True
+    self.signup_time = time.time()
+    self.signup_ip = signup_ip
+    self.username = username
+    self.password_hash = password_hash
+    self.name = name
+    self.email = email
+    self.api_key = util.testing.random_alphanumeric_string(length=64)
 
-    @staticmethod
-    def is_authenticated():
-        return True
+  @staticmethod
+  def is_authenticated():
+    return True
 
-    @staticmethod
-    def is_anonymous():
-        return False
+  @staticmethod
+  def is_anonymous():
+    return False
 
-    def get_id(self):
-        return unicode(self.user_id)
+  def get_id(self):
+    return unicode(self.user_id)
